144e1711e8d6b1204e250e37f0010618c51f55c7
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-07-19 16:34:00 -0500

    completed

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.static('client'));
 
 // body parser makes it possible to post JSON to the server
 // we can accss data we post on as req.body
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
@@ -22,6 +22,48 @@ var lions = [];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+app.get('/lions', (req, res) => res.json(lions));
+
+app.get('/lions/:id', (req, res) => {
+    let Lion = _.find(lions, { id: req.params.id })
+    res.json(lion || {})
+});
+
+app.post('/lions', (req, res) => {
+    let Lion = req.body;
+    id++;
+    lion.id = id + '';
+
+    lions.push(lion);
+    res.json(lion);
+});
+
+app.put('/lions', (req, res) => {
+    let update = req.body;
+    if(update.id){
+        delete update.id
+    }
+    let lion = _.findIndex(lions, { id: req.params.id })
+    if(!lions[lion]){
+        res.send();
+    }else{
+        let updatedLion = _.assign(lions[lion], update);
+        res.json(updatedLion);
+    }
+
+})
+app.delete('/lions/:id', (req, res) => {
+    let lion = _.findIndex(lions, { id: req.params.id })
+    if (!lions[lion]) {
+        res.send();
+    }else{
+        let deletedLion = lions[lion];
+        lions.splice(lion, 1);
+        res.json(deletedLion);
+    }
+})
+
+
 
 app.listen(3000);
 console.log('on port 3000');
